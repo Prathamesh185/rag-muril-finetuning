906c2ad4b0749fdd8f9e76b35223657f48d0de99
Prathamesh Patil <prathameshkoph@gmail.com>
2026-08-12 06:29:56 +0530

    changed llm model to gemini flash 3.1 flash lite

--- llm.py
+++ llm.py
@@ -39,7 +39,7 @@ def local_llm(prompt):
 def gemini_llm(prompt):
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3.1-flash-lite",
         contents=prompt,
     )
 
